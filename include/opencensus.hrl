318249a2a9cc2bb231263ba7fbabc039a3d15172
Tristan Sloughter <t@crashfast.com>
2017-09-04 18:01:14 -0700

    add attributes, annotations and links to span record

--- include/opencensus.hrl
+++ include/opencensus.hrl
@@ -1,29 +1,37 @@
 -record(trace_context, {
-          trace_id   :: opencensus:trace_id(),
-          span_id    :: opencensus:span_id(),
-          enabled    :: boolean(),
+          trace_id   :: opencensus:trace_id() | undefined,
+          span_id    :: opencensus:span_id() | undefined,
+          enabled    :: boolean() | undefined,
 
           sampler    :: module(),
           reporter   :: module(),
           propagator :: module()
-}).
+         }).
 
 -record(span, {
-          %% strt timestamp
+          %% start timestamp
           timestamp  :: opencensus:time_us(),
-          %% 64 bit int trace id
+          %% 128 bit int trace id
           trace_id   :: opencensus:trace_id() | undefined,
           %% name of the span
-          name       :: opencensus:info(),
+          name       :: unicode:chardata(),
           %% 64 bit int span id
           id         :: opencensus:span_id() | undefined,
           %% 64 bit int parent span
           parent_id  :: opencensus:span_id() | undefined,
-          %% Tags
-          tags = #{} :: otter:tags(),
-          %%  logs
-          logs = []  :: [{opencensus:info(), opencensus:info(),
-                          opencensus:service()}],
           %% microseconds between span start/end
-          duration   :: opencensus:time_us()
-}).
+          duration   :: opencensus:time_us(),
+
+          annotations :: opencensus:annotations(),
+          attributes  :: opencensus:attributes(),
+
+          %% link to a span in another trace
+          links       :: opencensus:links()
+         }).
+
+-record(link, {
+          type :: opencensus:link_type(),
+          trace_id :: opencensus:trace_id(),
+          span_id    :: opencensus:span_id(),
+          attributes  :: opencensus:attributes()
+         }).
--- src/opencensus.erl
+++ src/opencensus.erl
@@ -4,34 +4,103 @@
 %%%-------------------------------------------------------------------
 -module(opencensus).
 
--export([generate_trace_id/0, generate_span_id/0]).
+-export([start_span/3,
+         child_span/2,
+
+         timestamp/0,
+         generate_trace_id/0,
+         generate_span_id/0]).
 
 -include("opencensus.hrl").
 
--export_type([info/0, service/0, trace_id/0, span_id/0,
+-export_type([trace_id/0,
+              span_id/0,
               trace_context/0,
-              span/0, maybe_span/0, tags/0, maybe/1]).
+              span/0]).
 
--type info()          :: binary() | iolist() | atom() | integer().
--type ip4()           :: {0..255, 0..255, 0..255, 0..255}.
--type service()       :: binary() | list() | default | undefined | {binary() | list(), ip4(), integer()}.
 -type trace_id()      :: non_neg_integer().
 -type span_id()       :: non_neg_integer().
--type tag()           :: {info(), service()} | binary() | string() | atom().
--type tags()          :: #{binary() => tag()}.
 -type trace_context() :: #trace_context{}.
 -type span()          :: #span{}.
--type maybe(B)        :: B | undefined.
--type maybe_span()    :: maybe(span()).
+
+-type annotations()   :: maps:map(unicode:chardata(), attribute_value()).
+-type attributes()    :: maps:map(unicode:chardata(), attribute_value()).
+-type attribute_value() :: unicode:chardata() | boolean() | integer().
+
+%% A link requires a type which describes the relationship with the linked span
+%% and the identifiers of the linked span.
+%% Can be used, for example, in batching operations, where a single batch handler
+%% processes multiple requests from different traces.
+-type link() :: #link{}.
+-type link_type() :: child_linked_span | parent_linked_span.
+-type links() :: [link()].
+
+-type maybe(T)        :: T | undefined.
+
+%% label keys must be less than 128 bytes.
+%% label values must be less than 16 kilobytes.
+-type labels()        :: maps:map().
 
 %% timestamp in microseconds
 -type time_us() :: non_neg_integer().
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts a new span with a given Trace ID and Parent ID.
+%% @end
+%%--------------------------------------------------------------------
+-spec start_span(unicode:chardata(), maybe(integer()), maybe(integer())) -> maybe(span()).
+start_span(_Name, undefined, undefined) ->
+    undefined;
+start_span(Name, TraceId, ParentId) when is_integer(TraceId)
+                                       , (is_integer(ParentId)
+                                         orelse ParentId =:= undefined) ->
+    #span{timestamp = timestamp(),
+          trace_id = TraceId,
+          id = generate_span_id(),
+          parent_id = ParentId,
+          name = Name}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts a new span as a child of a existing span, using the parents
+%% Trace ID or a `{trace_id, parent_id}` tuple and setting the childs
+%% parent to the parents Span ID
+%% @end
+%%--------------------------------------------------------------------
+-spec child_span(unicode:chardata(), maybe(span()) |
+                {TraceID::trace_id(), SpanID::span_id()}) -> maybe(span()).
+child_span(_Name, undefined) ->
+    undefined;
+child_span(Name, #span{trace_id = TraceId, parent_id = ParentId}) ->
+    start_span(Name, TraceId, ParentId);
+child_span(Name, {TraceId, ParentId}) ->
+    start_span(Name, TraceId, ParentId).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the current system time in microseconds.
+%% @end
+%%--------------------------------------------------------------------
+-spec timestamp() -> time_us().
+timestamp() ->
+    erlang:system_time(microsecond).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates a 128 bit random integer to use as a trace id.
+%% @end
+%%--------------------------------------------------------------------
 -spec generate_trace_id() -> non_neg_integer().
 generate_trace_id() ->
     <<Id:128>> = crypto:strong_rand_bytes(16),
     Id.
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates a 64 bit random integer to use as a span id.
+%% @end
+%%--------------------------------------------------------------------
 -spec generate_span_id() -> non_neg_integer().
 generate_span_id() ->
     <<Id:64>> = crypto:strong_rand_bytes(8),
