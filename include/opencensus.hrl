67be201aa6d81fd1bc1df4ae92791fa86db28814
Tristan Sloughter <t@crashfast.com>
2017-09-05 20:16:36 -0700

    add more span fields

--- include/opencensus.hrl
+++ include/opencensus.hrl
@@ -9,14 +9,14 @@
          }).
 
 -record(span, {
-          %% start timestamp
-          timestamp  :: opencensus:time_us(),
+          start_time  :: opencensus:time_us(),
+          end_time  :: opencensus:time_us() | undefined,
           %% 128 bit int trace id
           trace_id   :: opencensus:trace_id() | undefined,
           %% name of the span
           name       :: unicode:chardata(),
           %% 64 bit int span id
-          id         :: opencensus:span_id() | undefined,
+          span_id    :: opencensus:span_id() | undefined,
           %% 64 bit int parent span
           parent_id  :: opencensus:span_id() | undefined,
           %% microseconds between span start/end
@@ -25,13 +25,78 @@
           annotations :: opencensus:annotations(),
           attributes  :: opencensus:attributes(),
 
-          %% link to a span in another trace
-          links       :: opencensus:links()
+          %% links to spans in other traces
+          links       :: opencensus:links(),
+
+          time_events :: opencensus:time_events(),
+
+          %% An optional final status for this span.
+          status      :: undefined,
+
+          %% A highly recommended but not required flag that identifies when a trace
+          %% crosses a process boundary. True when the parent_span belongs to the
+          %% same process as the current span.
+          %same_process_as_parent_span :: boolean(),
+
+          %% An optional number of child spans that were generated while this span
+          %% was active. If set, allows implementation to detect missing child spans.
+          child_span_count = 0 :: integer()
          }).
 
 -record(link, {
+          %% The relationship of the current span relative to the linked span:
+          %% child, parent, or unspecified.
           type :: opencensus:link_type(),
           trace_id :: opencensus:trace_id(),
           span_id    :: opencensus:span_id(),
           attributes  :: opencensus:attributes()
+         }).
+
+-record(links, {
+          links :: [opencensus:link()],
+
+          %% The number of dropped links after the maximum size was enforced. If
+          %% this value is 0, then no links were dropped.
+          dropped_links_count :: integer()
+         }).
+
+-record(time_event, {
+          time :: opencensus:time_us(),
+          value :: opencensus:annotation() | opencensus:network_event()
+         }).
+
+-record(network_event, {
+          time :: opencensus:time_us(),
+
+          %% type of NetworkEvent. Indicates whether the RPC message was sent or received.
+          type :: opencensus:network_event_type(),
+
+          %% identifier for the message, which must be unique in this span.
+          message_id :: integer(),
+
+          %% number of uncompressed bytes sent or received
+          uncompressed_message_size :: integer(),
+
+          %% number of compressed bytes sent or received
+          compressed_message_size :: integer()
+         }).
+
+-record(time_events, {
+          %% A collection of `TimeEvent`s.
+          time_event :: [opencensus:time_event()],
+
+          %% The number of dropped annotations in all the included time events.
+          %% If the value is 0, then no annotations were dropped.
+          dropped_annotations_count :: integer(),
+
+          %% The number of dropped network events in all the included time events.
+          %% If the value is 0, then no network events were dropped.
+          dropped_network_events_count :: integer()
+         }).
+
+-record(status, {
+          code :: integer(),
+          %% developer-facing error message
+          message :: unicode:chardata(),
+          details :: [term()]
          }).
