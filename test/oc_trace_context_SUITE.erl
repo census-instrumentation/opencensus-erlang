4fb233a1fb7bbcfc2f08b7be3ed36f6b2ff35883
Heinz N. Gies <heinz@licenser.net>
2017-09-14 19:11:22 +0200

    Add elvis config & cleanup
    Adds an elvis config and some cleanup of the suite to not vomit
    all over the place and hide issues that might be worth discussing.
    
    The linter does not pass on this point, this is on purpose as we
    should discuss if what rules we want to impose plus 2 errors are
    based on missing callbacks.

--- test/oc_trace_context_SUITE.erl
+++ test/oc_trace_context_SUITE.erl
@@ -12,7 +12,7 @@
 -include("opencensus.hrl").
 
 all() ->
-    [encode_decode, decode_with_extra_junk, encode_decode_headers].
+    [encode_decode,  decode_with_extra_junk,  encode_decode_headers].
 
 init_per_suite(Config) ->
     Config.
@@ -20,83 +20,86 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
     ok.
 
-init_per_testcase(_, Config) ->
+init_per_testcase(_,  Config) ->
     Config.
 
-end_per_testcase(_, _Config) ->
+end_per_testcase(_,  _Config) ->
     ok.
 
+%% TraceId: 85409434994488837557643013731547696719
+%% SpanId: 7017280452245743464
+%% Enabled: true
+-define(EXAMPLE_BIN, <<0, 0, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+        79, 1, 97, 98, 99, 100, 101, 102, 103, 104, 2, 1>>).
+
+reencode(Binary) ->
+    {ok,  Decoded} = oc_trace_context_binary:decode(Binary),
+    {ok,  Encoded} = oc_trace_context_binary:encode(Decoded),
+    Encoded.
+
 encode_decode(_Config) ->
-    %% TraceId: 85409434994488837557643013731547696719
-    %% SpanId: 7017280452245743464
-    %% Enabled: true
-    Binary = <<0,0,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,1,97,98,99,100,101,102,103,104,2,1>>,
-    {ok, Decoded} = oc_trace_context_binary:decode(Binary),
-    {ok, Encoded} = oc_trace_context_binary:encode(Decoded),
-    ?assertMatch(Binary, Encoded),
+    Encoded = reencode(?EXAMPLE_BIN),
 
-    InvalidBothIdsBinary = <<0:8,0:8,0:128,1:8,0:64,2:8,1>>,
-    ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidBothIdsBinary)),
+    ?assertMatch(?EXAMPLE_BIN,  Encoded),
 
-    InvalidTraceIdBinary = <<0:8,0:8,0:128,1:8,1:64,2:8,1>>,
-    ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidTraceIdBinary)),
+    InvalidBothIdsBinary = <<0:8, 0:8, 0:128, 1:8, 0:64, 2:8, 1>>,
+    ?assertEqual({error,  invalid},  oc_trace_context_binary:decode(InvalidBothIdsBinary)),
 
-    InvalidSpanIdBinary = <<0:8,0:8,1:128,1:8,0:64,2:8,1>>,
-    ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidSpanIdBinary)).
+    InvalidTraceIdBinary = <<0:8, 0:8, 0:128, 1:8, 1:64, 2:8, 1>>,
+    ?assertEqual({error,  invalid},  oc_trace_context_binary:decode(InvalidTraceIdBinary)),
+
+    InvalidSpanIdBinary = <<0:8, 0:8, 1:128, 1:8, 0:64, 2:8, 1>>,
+    ?assertEqual({error,  invalid},  oc_trace_context_binary:decode(InvalidSpanIdBinary)).
 
 
 decode_with_extra_junk(_Config) ->
-    %% TraceId: 85409434994488837557643013731547696719
-    %% SpanId: 7017280452245743464
-    %% Enabled: true
-    Binary = <<0,0,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,1,97,98,99,100,101,102,103,104,2,1>>,
-    BinaryWithJunk = <<Binary/binary,23,4,5>>,
-    {ok, Decoded} = oc_trace_context_binary:decode(BinaryWithJunk),
-    {ok, Encoded} = oc_trace_context_binary:encode(Decoded),
+    Binary = ?EXAMPLE_BIN,
+    BinaryWithJunk = <<Binary/binary, 23, 4, 5>>,
+    Encoded = reencode(BinaryWithJunk),
 
-    ?assertMatch(Binary, Encoded).
+    ?assertMatch(Binary,  Encoded).
 
 encode_decode_headers(_Config) ->
     %% TraceId: 4bf92f3577b34da6a3ce929d0e0e4736
     %% SpanId: 00f067aa0ba902b7
     %% Enabled: true
     Header = <<"00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01">>,
-    {ok, Decoded} = oc_trace_context_headers:decode(Header),
-    {ok, Encoded} = oc_trace_context_headers:encode(Decoded),
-    ?assertEqual(Header, list_to_binary(Encoded)),
-    ?assertEqual({ok, Decoded}, oc_trace_context_headers:decode(Encoded)),
+    {ok,  Decoded} = oc_trace_context_headers:decode(Header),
+    {ok,  Encoded} = oc_trace_context_headers:encode(Decoded),
+    ?assertEqual(Header,  list_to_binary(Encoded)),
+    ?assertEqual({ok,  Decoded},  oc_trace_context_headers:decode(Encoded)),
 
     %% TraceId: 4bf92f3577b34da6a3ce929d0e0e4736
     %% SpanId: 00f067aa0ba902b7
     %% Enabled: false
     DisabledHeader = <<"00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-00">>,
-    {ok, DisabledDecoded} = oc_trace_context_headers:decode(DisabledHeader),
-    {ok, DisabledEncoded} = oc_trace_context_headers:encode(DisabledDecoded),
-    ?assertEqual(DisabledHeader, list_to_binary(DisabledEncoded)),
-    ?assertEqual({ok, DisabledDecoded}, oc_trace_context_headers:decode(DisabledEncoded)),
+    {ok,  DisabledDecoded} = oc_trace_context_headers:decode(DisabledHeader),
+    {ok,  DisabledEncoded} = oc_trace_context_headers:encode(DisabledDecoded),
+    ?assertEqual(DisabledHeader,  list_to_binary(DisabledEncoded)),
+    ?assertEqual({ok,  DisabledDecoded},  oc_trace_context_headers:decode(DisabledEncoded)),
     ?assertNot(DisabledDecoded#trace_context.enabled),
 
     %% Decode invalid headers
     InvalidSpanIdHeader = <<"00-4bf92f3577b34da6a3ce929d0e0e4736-0000000000000000-00">>,
-    {error, invalid} = oc_trace_context_headers:decode(InvalidSpanIdHeader),
+    {error,  invalid} = oc_trace_context_headers:decode(InvalidSpanIdHeader),
 
     InvalidTraceIdHeader = <<"00-00000000000000000000000000000000-00f067aa0ba902b7-00">>,
-    {error, invalid} = oc_trace_context_headers:decode(InvalidTraceIdHeader),
+    {error,  invalid} = oc_trace_context_headers:decode(InvalidTraceIdHeader),
 
     InvalidBothIdsHeader = <<"00-00000000000000000000000000000000-0000000000000000-00">>,
-    {error, invalid} = oc_trace_context_headers:decode(InvalidBothIdsHeader),
+    {error,  invalid} = oc_trace_context_headers:decode(InvalidBothIdsHeader),
 
 
     %% Encode invalid trace contexts
     InvalidTC = #trace_context{trace_id = 0,
                                span_id = 0,
                                enabled = false},
-    {error, invalid} = oc_trace_context_headers:encode(InvalidTC),
+    {error,  invalid} = oc_trace_context_headers:encode(InvalidTC),
 
     InvalidTraceIdTC = #trace_context{trace_id = 85409434994488837557643013731547696719,
                                       span_id = 0,
                                       enabled = true},
-    {error, invalid} = oc_trace_context_headers:encode(InvalidTraceIdTC),
+    {error,  invalid} = oc_trace_context_headers:encode(InvalidTraceIdTC),
 
     InvalidSpanIdTC = #trace_context{trace_id = 0,
                                      span_id = 7017280452245743464,
