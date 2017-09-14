9254272ce5727b74865981a4923aa9ce59140dd4
Heinz N. Gies <heinz@licenser.net>
2017-09-15 01:16:43 +0200

    Elvis (#13)
    * Add linter plugin
    * Add elvis config
    * cleanup linter violations
    * Add  linter to circleci

--- test/oc_trace_context_SUITE.erl
+++ test/oc_trace_context_SUITE.erl
@@ -26,33 +26,36 @@ init_per_testcase(_, Config) ->
 end_per_testcase(_, _Config) ->
     ok.
 
-encode_decode(_Config) ->
-    %% TraceId: 85409434994488837557643013731547696719
-    %% SpanId: 7017280452245743464
-    %% Enabled: true
-    Binary = <<0,0,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,1,97,98,99,100,101,102,103,104,2,1>>,
+%% TraceId: 85409434994488837557643013731547696719
+%% SpanId: 7017280452245743464
+%% Enabled: true
+-define(EXAMPLE_BIN, <<0, 0, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+        79, 1, 97, 98, 99, 100, 101, 102, 103, 104, 2, 1>>).
+
+reencode(Binary) ->
     {ok, Decoded} = oc_trace_context_binary:decode(Binary),
     {ok, Encoded} = oc_trace_context_binary:encode(Decoded),
-    ?assertMatch(Binary, Encoded),
+    Encoded.
 
-    InvalidBothIdsBinary = <<0:8,0:8,0:128,1:8,0:64,2:8,1>>,
+encode_decode(_Config) ->
+    Encoded = reencode(?EXAMPLE_BIN),
+
+    ?assertMatch(?EXAMPLE_BIN, Encoded),
+
+    InvalidBothIdsBinary = <<0:8, 0:8, 0:128, 1:8, 0:64, 2:8, 1>>,
     ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidBothIdsBinary)),
 
-    InvalidTraceIdBinary = <<0:8,0:8,0:128,1:8,1:64,2:8,1>>,
+    InvalidTraceIdBinary = <<0:8, 0:8, 0:128, 1:8, 1:64, 2:8, 1>>,
     ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidTraceIdBinary)),
 
-    InvalidSpanIdBinary = <<0:8,0:8,1:128,1:8,0:64,2:8,1>>,
+    InvalidSpanIdBinary = <<0:8, 0:8, 1:128, 1:8, 0:64, 2:8, 1>>,
     ?assertEqual({error, invalid}, oc_trace_context_binary:decode(InvalidSpanIdBinary)).
 
 
 decode_with_extra_junk(_Config) ->
-    %% TraceId: 85409434994488837557643013731547696719
-    %% SpanId: 7017280452245743464
-    %% Enabled: true
-    Binary = <<0,0,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,1,97,98,99,100,101,102,103,104,2,1>>,
-    BinaryWithJunk = <<Binary/binary,23,4,5>>,
-    {ok, Decoded} = oc_trace_context_binary:decode(BinaryWithJunk),
-    {ok, Encoded} = oc_trace_context_binary:encode(Decoded),
+    Binary = ?EXAMPLE_BIN,
+    BinaryWithJunk = <<Binary/binary, 23, 4, 5>>,
+    Encoded = reencode(BinaryWithJunk),
 
     ?assertMatch(Binary, Encoded).
 
