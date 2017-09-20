b77ad5ee3fcd56b3d4d0f5438e16db781b5623e6
Tristan Sloughter <t@crashfast.com>
2017-09-19 18:49:13 -0700

    use rand:uniform for faster random num generation

--- src/opencensus.erl
+++ src/opencensus.erl
@@ -185,8 +185,7 @@ put_attributes(NewAttributes, Span=#span{attributes=Attributes}) ->
 %%--------------------------------------------------------------------
 -spec generate_trace_id() -> non_neg_integer().
 generate_trace_id() ->
-    <<Id:128>> = crypto:strong_rand_bytes(16),
-    Id.
+    uniform(2 bsl 127). %% 2 shifted left by 127 == 2 ^ 128
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -195,5 +194,22 @@ generate_trace_id() ->
 %%--------------------------------------------------------------------
 -spec generate_span_id() -> non_neg_integer().
 generate_span_id() ->
-    <<Id:64>> = crypto:strong_rand_bytes(8),
-    Id.
+    uniform(2 bsl 63). %% 2 shifted left by 63 == 2 ^ 64
+
+%%
+
+%% Before OTP-20 rand:uniform could not give precision higher than 2^56.
+%% Here we do a compile time check for support of this feature and will
+%% combine multiple calls to rand if on an OTP version older than 20.0
+-ifdef(high_bit_uniform).
+uniform(X) ->
+    rand:uniform(X).
+-else.
+-define(TWO_POW_56, 2 bsl 55).
+
+uniform(X) when X =< ?TWO_POW_56 ->
+    rand:uniform(X);
+uniform(X) ->
+    R = rand:uniform(?TWO_POW_56),
+    (uniform(X bsr 56) bsl 56) + R.
+-endif.
