245b2a81d488e84f548b5694ed38744fd0f0591c
Ilya Khaprov <i.khaprov@gmail.com>
2018-03-26 00:37:34 +0300

    add Apache 2 header to aggregations and brief aggregations descriptions

--- src/oc_stat_aggregation_count.erl
+++ src/oc_stat_aggregation_count.erl
@@ -1,3 +1,25 @@
+%%%------------------------------------------------------------------------
+%% Copyright 2018, OpenCensus Authors
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% @doc
+%% Count indicates that data collected and aggregated
+%% with this method will be turned into a count value.
+%% For example, total number of accepted requests can be
+%% aggregated by using Count.
+%% @end
+%%%-----------------------------------------------------------------------
+
 -module(oc_stat_aggregation_count).
 
 -include("opencensus.hrl").
